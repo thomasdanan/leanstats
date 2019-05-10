c954b58a6d560442e4846fc91653d663d6df0fbb
thomasdanan <thomas.danan@scality.com>
2019-05-10 12:48:42 +0200

    sort result by issue start date and add issue title

--- github/GHIssues.py
+++ github/GHIssues.py
@@ -10,6 +10,25 @@ from dateutil.parser import parse
 from GHClient import GHClient
 from GHUtils import GHUtils
 
+
+class ScalityIssue:
+    issue = None
+    complexity = None
+    events = None
+    def __init__(self, issue, complexity, events):
+        self.issue = issue
+        self.complexity = complexity
+        self.events = events
+
+    def getComplexity(self):
+        return self.complexity
+
+    def getIssue(self):
+        return self.issue
+
+    def getStartDate(self):
+        return GHUtils.getEventDate(self.events,'assigned')
+
 class GHIssues:
 
     ghClient = None
@@ -17,19 +36,35 @@ class GHIssues:
     def __init__(self, ghClient):
         self.ghClient = ghClient
 
-    def parseIssues(self, prs, complexity):
-        print "number;complexity;assignee;start;end;elapsed"
+    def filterClosedInProgress(self, issues):
+        closedInProgress = []
         for issue in issues:
             #tfm = time for merge: time between ready for review and merge
             number = issue['number']
             state = issue['state']
             assignee = issue['assignee']
+            # only consider closed and in progress (i.e. assignee not None)
             if state == 'open' and assignee == None:
                 continue
-            eventsUrl = issue['events_url']
-            events = ghClient.collectItems(eventsUrl)
-            start = GHUtils.getEventDate(events,'assigned')
+            else:
+                eventsUrl = issue['events_url']
+                events = ghClient.collectItems(eventsUrl)
+                scalityIssue = ScalityIssue(issue, complexity, events)
+                closedInProgress.append(scalityIssue)
+        return closedInProgress
+
+    def printIssuesSummary(self, scalityIssues):
+        # sort by issue start date
+        scalityIssues.sort(key=ScalityIssue.getStartDate)
+        for scalityIssue in scalityIssues:
+            issue = scalityIssue.getIssue()
+            complexity = scalityIssue.getComplexity()
+            number = issue['number']
+            state = issue['state']
+            assignee = issue['assignee']
+            start = scalityIssue.getStartDate()
             end = issue['closed_at']
+            # if issue is opened, no end date so elapsed is time between start date and now
             if state == 'open':
                 elapsedHours = GHUtils.getDeltaWEExcluded(start,datetime.datetime.now())
                 end = 'None'
@@ -38,16 +73,17 @@ class GHIssues:
                 end = GHUtils.getDayDate(parse(end))
 
             elapsedDays = "%.1f" % (elapsedHours / 24.0)
-
-            print str(number)+";"+complexity+";"+str(assignee['login'])+";"+GHUtils.getDayDate(start)+";"+end+";"+elapsedDays
+            print str(number)+";"+issue['title']+";"+complexity+";"+str(assignee['login'])+";"+GHUtils.getDayDate(start)+";"+end+";"+elapsedDays
 
 user = raw_input("github id: ")
 #user = 'thomasdanan'
 passwd = getpass.getpass()
 ghClient = GHClient(user, passwd)
 ghIssues = GHIssues(ghClient)
+closedInProgress = []
 complexities = {'easy', 'medium', 'hard'}
 for complexity in complexities:
     url = "https://api.github.com/search/issues?q=is:issue+repo:scality/metalk8s+milestone:\"MetalK8s 2.0.0-alpha4\"+label:"+complexity+"&per_page=100"
     issues = ghClient.collectItems(url)
-    ghIssues.parseIssues(issues, complexity)
+    closedInProgress = closedInProgress + ghIssues.filterClosedInProgress(issues)
+ghIssues.printIssuesSummary(closedInProgress)
